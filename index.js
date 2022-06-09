d2ecdb0aef6b0d363d4f838016b5a0296c3762eb
Socks <patricksutcliffe123@gmail.com>
2022-06-08 20:14:43 -0400

    added poulated data in list elements

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ function submitAction() {
     const email = document.forms["contactForm"]["coEmail"].value
     const fName = document.forms["contactForm"]["firName"].value
     const lName = document.forms["contactForm"]["lasName"].value
+    const messA = document.forms["contactForm"]["messageA"].value
     if(email == ""||fName==""||lName == ""){
         alert("Please fill out all the fields")
         return false
     } else{
         console.log("hello from the other side")
-        localStorage.setItem("fname", document.forms["contactForm"]["firName"].value)
-        localStorage.setItem("lname", document.forms["contactForm"]["lasName"].value)
+        localStorage.setItem("fname", fName)
+        localStorage.setItem("lname", lName)
+        localStorage.setItem("email", email)
+        localStorage.setItem("message", messA)
         location.href='./next.html'
     }
 
--- next.js
+++ next.js
@@ -2,5 +2,10 @@
 console.log(localStorage.getItem('fname'))
 
 function populateForm(){
-
+    document.getElementById("first").innerHTML = localStorage.getItem('fname')
+    document.getElementById("last").innerHTML = localStorage.getItem('lname')
+    document.getElementById("email").innerHTML = localStorage.getItem('email')
+    document.getElementById("message").innerHTML = localStorage.getItem('message')
 }
+
+populateForm();
